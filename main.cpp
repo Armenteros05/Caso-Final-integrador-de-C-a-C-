d5fdee682b04023690bf3509c5c19567e493090c
Armenteros05 <sarmerod@myuax.com>
2023-11-30 12:12:36 +0100

    codigo mejorado con banderas de error

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <filesystem>
 
 using namespace std;
+namespace fs = filesystem;
 
 struct ColorConsole
 {
@@ -18,26 +20,35 @@ struct ConsoleBox
 
 ConsoleBox *consoleBox = new ConsoleBox; // suponemos que ya está inicializado
 
+void print_current_directory()
+{
+    fs::path current_path = fs::current_path();
+    cout << "Directorio actual: " << current_path << endl;
+}
+
 bool file_exists(const string &filename)
 {
     ifstream file(filename);
     return file.good();
 }
 
-void load_script(const string &filename, bool show_script = false)
+// Modificamos la función para que devuelva un valor indicando éxito o fracaso
+bool load_script(const char* filename, bool show_script = false)
 {
+    print_current_directory();  // Imprimir el directorio actual
+
     if (!file_exists(filename))
     {
-        cerr << "El archivo '" << filename << "' no existe." << endl;
-        return;
+        cerr << "Error: El archivo '" << filename << "' no existe." << endl;
+        return false;
     }
 
     string script;
     ifstream file(filename, ios::binary);
     if (!file.is_open())
     {
-        cerr << "Error al abrir el archivo '" << filename << "'" << endl;
-        return;
+        cerr << "Error: No se pudo abrir el archivo '" << filename << "'" << endl;
+        return false;
     }
 
     char buf[4001];
@@ -49,6 +60,12 @@ void load_script(const string &filename, bool show_script = false)
 
     file.close();
 
+    if (file.fail() && !file.eof())
+    {
+        cerr << "Error: Fallo durante la lectura del archivo '" << filename << "'" << endl;
+        return false;
+    }
+
     if (show_script)
     {
         cout << ColorConsole::fg_blue << ColorConsole::bg_white;
@@ -56,6 +73,8 @@ void load_script(const string &filename, bool show_script = false)
     }
     consoleBox->new_text();
     consoleBox->set_text(script);
+
+    return true;  // Indicar éxito en la carga del archivo
 }
 
 void add_script()
@@ -67,7 +86,7 @@ void add_script()
 
     if (file_exists(filename))
     {
-        cerr << "El archivo '" << filename << "' ya existe. Utiliza un nombre diferente." << endl;
+        cerr << "Error: El archivo '" << filename << "' ya existe. Utiliza un nombre diferente." << endl;
         return;
     }
 
@@ -92,6 +111,13 @@ int main()
     cout << "Introduce el nombre del archivo a cargar: ";
     string filename;
     getline(cin, filename);
-    load_script(filename, true);
+
+    // Modificado para usar la bandera de error
+    if (!load_script(filename.c_str(), true))
+    {
+        // Manejar el error según los requisitos
+        return 1;  // Código de error para indicar fallo en la carga del archivo
+    }
+
     return 0;
 }
